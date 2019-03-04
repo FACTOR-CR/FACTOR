aa9651c3b3385bf362ee7a1f57c9bfc777845762
FACTOR-CR <47648461+FACTOR-CR@users.noreply.github.com>
2019-03-04 20:32:21 +0100

    Add files via upload

--- starting_kit_hadaca/sample_code_submission/model.py
+++ starting_kit_hadaca/sample_code_submission/model.py
@@ -1,92 +1,155 @@
 '''
-Sample predictive model.
-You must supply at least 4 methods:
+Our classifier model : based on the multiclass perceptron
 - fit: trains the model.
-- predict: uses the model to perform predictions.
-- save: saves the model.
-- load: reloads the model.
+- predict : predict the class of datas
 '''
-import pickle
-import numpy as np   # We recommend to use numpy arrays
-from os.path import isfile
-from sklearn.base import BaseEstimator
-from sklearn.svm import SVC
+
+import numpy as np
 from sklearn.decomposition import TruncatedSVD
 
-class model (BaseEstimator):
+#Fonction utilitaires
+def scalaire(x1, x2):
+    '''
+    Fonction qui retroune le produit scalaire 
+    des deux vecteurs x1 et x2
+    '''
+    res = 0
+    for i in range(x2.size) :
+        res = res + (x1[i]*x2[i])
+    return res 
+def maxi (t) :
+    '''
+    Renvoie le maximun d'un tableau tab
+    '''
+    maxi = t[0]
+    for i in range(t.size) :
+        if t[i] > maxi :
+            maxi = t[i]
+    return maxi
+def mini (t) :
+    '''
+    Renvoie le minimum d'un tableau tab
+    '''
+    mini = t[0]
+    for i in range(t.size) :
+        if t[i] < mini :
+            mini = t[i]
+    return mini
+
+def probas(X, w) :
+    '''
+    Fonction qui permet de calculer la probabilité qu'une donnée data 
+    appartienne à une classe, et qui renvoie le tableau de probabilités
+    pour chaque classe.
+    '''
+    preds = np.zeros(10)
+    scals = np.zeros(10)
+    total = 0
+    Max = 0
+    A = 0
+        
+    for classe in range(10) :
+        scals[classe] = scalaire(w[classe],X)
+            
+    Max = maxi(scals)
+        
+    for classe in range(10) :
+        A = scals[classe] - Max
+        total = total + np.exp(A)
+            
+    for classe in range(10) :
+        A = scals[classe] - Max
+        preds[classe] = np.exp(A) / total
+    return preds
+    
+def maxIndice(preds) :
+    '''
+    Fonction qui renvoie l'indice de la case contenant 
+    la valeur maximum d'un tableau preds
+    '''
+    indice = 0
+    prediction = preds[0]
+    for i in range(1,preds.size) :
+        if (preds[i] > prediction) :
+            prediction = preds[i]
+            indice = i
+    return indice
+    
+def updatew(X,y,w,preds) :
+    '''
+    Fonction qui permet de mettre à jour w, les poids, pour une donnée data et sa réference ref
+    en fonction du tableau de prediction preds
+    '''
+    for classe in range(10) :
+        for j in range(w.shape[1]) :
+            if (y == classe + 1) :
+                w[classe][j] += X[j]*(1-preds[classe])
+            else :
+                w[classe][j] -= X[j]*preds[classe]
+    return w
+     
+def epoch(X,y,w) :
+    '''
+    Fonction epoch qui cherche pour chaque donnée la probabilité maximum et donc
+    predit la classe de la donnée. Si la prédiction est fausse elle augmente le 
+    nombre d'erreurs et appelle la fonction qui met a jour w.
+    Elle renvoie le nombre d'erreur à chaque époque
+    '''
+    nbErr = 0
+    x = 0
+    preds = np.zeros(10)
+    for i in range (X.shape[0]) :
+        preds = probas(X[i], w)
+        x = maxIndice(preds)
+        if (x+1 != y[i]) :
+            nbErr = nbErr + 1
+            w = updatew(X[i], y[i], w, preds)
+    return nbErr , w
+    
+    
+#Classifier
+class model () :
     def __init__(self):
         '''
-        This constructor is supposed to initialize data members.
-        Use triple quotes for function documentation. 
+        This constructor initialize data members. 
         '''
-        self.num_train_samples=0
-        self.num_feat=1
-        self.num_labels=1
+        self.NBEPOCH = 10
+        self.NBCLASSES = 10
         self.is_trained=False
         self.svd = TruncatedSVD(n_components = 10)
-        self.classifier= SVC(C=1.)
+        self.w = np.array([])
 
-    def fit(self, X, y):
+    
+    def fit(self,X,y) :
         '''
-        This function should train the model parameters.
-        Here we do nothing in this example...
-        Args:
-            X: Training data matrix of dim num_train_samples * num_feat.
-            y: Training label matrix of dim num_train_samples * num_labels.
-        Both inputs are numpy arrays.
-        For classification, labels could be either numbers 0, 1, ... c-1 for c classe
-        or one-hot encoded vector of zeros, with a 1 at the kth position for class k.
-        The AutoML format support on-hot encoding, which also works for multi-labels problems.
-        Use data_converter.convert_to_num() to convert to the category number format.
-        For regression, labels are continuous values.
-        
-        
+        X: Training data matrix of dim num_train_samples * num_feat.
+        y: Training label matrix of dim num_train_samples * num_labels.
         '''
-        self.svd.fit(X)
-        X = self.svd.transform(X)
-        self.classifier.fit(X,y)
-        self.num_train_samples = X.shape[0]
-        if X.ndim>1: self.num_feat = X.shape[1]
-        print("FIT: dim(X)= [{:d}, {:d}]".format(self.num_train_samples, self.num_feat))
-        num_train_samples = y.shape[0]
-        if y.ndim>1: self.num_labels = y.shape[1]
-        print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
-        if (self.num_train_samples != num_train_samples):
-            print("ARRGH: number of samples in X and y do not match!")
-        self.is_trained=True
+        self.w = np.zeros((self.NBCLASSES,X.shape[1]))
+        
+        nbErr = 1
+        iteration = 1
+        while (iteration < self.NBEPOCH) :
+            print(iteration)
+            (nbErr, self.w) = epoch(X,y,self.w)
+            iteration = iteration + 1
 
-    def predict(self, X):
+    
+    def predict(self,X):
         '''
         This function should provide predictions of labels on (test) data.
-        Here we just return zeros...
-        Make sure that the predicted values are in the correct format for the scoring
-        metric. For example, binary classification problems often expect predictions
-        in the form of a discriminant value (if the area under the ROC curve it the metric)
-        rather that predictions of the class labels themselves. For multi-class or multi-labels
-        problems, class probabilities are often expected if the metric is cross-entropy.
-        Scikit-learn also has a function predict-proba, we do not require it.
-        The function predict eventually can return probabilities.
         '''
-#         print("Matrix Factorization of test set by SVD")
-#         svd = TruncatedSVD(n_components = 100)
-
-#         A = svd.fit_transform(X)
-#         T = svd.components_
-        
-#         print("Shape of A :", A.shape)
-#         print("Shape of T :", T.shape)
-        X = self.svd.transform(X)
-        num_test_samples = X.shape[0]
-        if X.ndim>1: num_feat = X.shape[1]
-        print("PREDICT: dim(X)= [{:d}, {:d}]".format(num_test_samples, num_feat))
-        if (self.num_feat != num_feat):
-            print("ARRGH: number of features in X does not match training data!")
-        print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
-        y = self.classifier.predict(X)
-        # If you uncomment the next line, you get pretty good results for the Iris data :-)
-        #y = np.round(X[:,3])
+        y = np.zeros(X.size)
+        scal = np.zeros(self.w.shape[0])
+        indice = 0
+        for i in range(X.shape[0]) :
+            for j in range(self.w.shape[0]) :
+                scal[j] = scalaire(self.w[j],X[i])
+            y[i] = maxIndice(scal)
         return y
-
+ 
+        
+        
     def save(self, path="./"):
         pickle.dump(self, open(path + '_model.pickle', "wb"))
 
